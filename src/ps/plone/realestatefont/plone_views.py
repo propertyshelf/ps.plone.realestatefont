67ac6574e2fbd7a8259ffdb5e105d79418495d0e
Thomas Massmann <thomas@propertyshelf.com>
2016-11-21 12:53:39 +0100

    Use custom view class to inject CSS resource for Plone 5.

---
+++ src/ps/plone/realestatefont/plone_views.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# zope imports
+from Products.CMFPlone.resources import add_resource_on_request
+from Products.Five import BrowserView
+from plone import api as ploneapi
+
+
+PLONE_5 = '4' < ploneapi.env.plone_version() < '6'
+
+
+class FontPreview(BrowserView):
+    """View to preview the font."""
+
+    def __call__(self):
+        self.setup()
+        return super(FontPreview, self).__call__()
+
+    def setup(self):
+        if PLONE_5:
+            add_resource_on_request(self.request, 'psplonerealestatefont')
